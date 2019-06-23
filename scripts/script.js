990454a582dd497c8549c84250ecde20ed1f3020
Nikita <cherevaty.nik@gmail.com>
2019-06-23 12:23:25 +0300

    fixedCode

--- scripts/script.js
+++ scripts/script.js
@@ -10,9 +10,8 @@ xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos', true);
 xhr.addEventListener('load', () => {
   xhrUsers.send();
   xhrUsers.addEventListener('load', () => {
-    let todosList = JSON.parse(xhr.response);
-    let usersList = JSON.parse(xhrUsers.response);
-    console.log(todosList, usersList);
+    const todosList = JSON.parse(xhr.response);
+    const usersList = JSON.parse(xhrUsers.response);
 
     const table = document.createElement('table');
     const thead = document.createElement('thead');
@@ -23,11 +22,9 @@ xhr.addEventListener('load', () => {
                           <th>Is Completed</th>
                        </tr>`;
     table.appendChild(thead);
-    console.log(table);
     for(let item of todosList) {
       let rowNode = document.createElement('tr');
       const user = usersList.find(userObj => userObj['id'] === item['userId']);
-      console.log(user);
       rowNode.innerHTML = `
       <td>
         ${item.title}
